22374b2e3576877c4cbafd0ec46d1e2294255f08
Mateusz Łokietek <mateusz.lokietek.001@gmail.com>
2022-02-12 20:28:58 +0100

    Time: 0 ms (100.00%), Space: 43.1 MB (20.02%) - LeetHub

---
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
@@ -0,0 +1,13 @@
+/*
+    Time Complexity:  O(n-1)
+    Space Complexity: O(1)
+*/
+class Solution {
+    public int[] runningSum(int[] nums) {
+        
+        for(int i = 1; i < nums.length; i++)
+            nums[i] += nums[i-1];
+        
+        return nums;
+    }
+}
