5b3081274b416d1351ac48c1ec52da3dbce4e1e0
Mateusz Łokietek <mateusz.lokietek.001@gmail.com>
2022-03-20 09:32:43 +0100

    Time: 0 ms (100.00%), Space: 42.6 MB (70.79%) - LeetHub

---
+++ 559-maximum-depth-of-n-ary-tree/559-maximum-depth-of-n-ary-tree.java
@@ -0,0 +1,40 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    public int maxDepth(Node root) {
+        
+        if(root == null)
+            return 0;
+        
+        int largest = 0;
+        int temp = 0;
+        
+        for(Node child : root.children)
+        {
+            temp = maxDepth(child);
+            
+            if(largest < temp)
+                largest = temp;
+        }
+        
+        return largest+1;
+        
+    }
+}
