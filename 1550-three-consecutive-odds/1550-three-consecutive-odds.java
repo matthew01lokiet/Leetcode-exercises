e33f74520af8dab8019090fd9e8fed1b60299636
Mateusz Łokietek <mateusz.lokietek.001@gmail.com>
2022-07-10 19:59:02 +0200

    Time: 0 ms (100.00%), Space: 42.6 MB (69.23%) - LeetHub

---
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        int odds = 0;
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] % 2 == 1)
+                odds++;
+            else
+                odds = 0;
+            
+            if(odds == 3)
+                return true;
+        }
+        return false;
+    }
+}
